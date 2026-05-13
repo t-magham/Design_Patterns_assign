b04a2e9baeb99c05e917d69bb9b35b080b43854c
taregh magham <tmagham@yahoo.com>
2026-05-13 11:30:05 +0300

    use Decorator to allow apply multiple discount chained, eg: student and firstTimeUser\nAlso we fixed lot of redundancy in the code overall and it has become more coherent

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ class Product:
 
     def set_price(self, new_price: float):
         self._price = new_price
+    def get_name(self):
+        return self._name
+    def set_name(self, new_name: str):
+        self._name = new_name
 
-class CartItem(object):
+class CartItem:
     def __init__(self, product: Product, quantity: int):
         self.product = product
         self.quantity = quantity
@@ -21,38 +25,52 @@ class Discount:
     def apply(self, price: float) -> float:
         raise NotImplementedError
 
-class StudentDiscount(Discount):
+class NoDiscount(Discount):
     def apply(self, price: float) -> float:
+        return price # base case for our decorator
+
+class DiscountDecorator(Discount):
+    def __init__(self, discount: Discount):
+        self.wrapped_discount = discount
+
+class StudentDiscount(DiscountDecorator):
+    def apply(self, price: float) -> float:
+        price = self.wrapped_discount.apply(price)
         return price * 0.7
 
-class FirstTimeUserDiscount(Discount):
+class FirstTimeUserDiscount(DiscountDecorator):
     def apply(self, price: float) -> float:
+        price = self.wrapped_discount.apply(price)
         return price * 0.5
 
 class DiscountFactory:
     @staticmethod
-    def create(discount_type: str) -> Discount:
-        if discount_type == "Student":
-            return StudentDiscount()
-        elif discount_type == "FirstTimeUser":
-            return FirstTimeUserDiscount()
-        raise ValueError(f"Unknown discount: {discount_type}")
+    def create(discount_types: list[str]) -> Discount:
+        discount = NoDiscount()
+        for t in discount_types:
+            if t == "Student":
+                discount = StudentDiscount(discount)
+            elif t == "FirstTimeUser":
+                discount = FirstTimeUserDiscount(discount)
+            else:
+                raise ValueError(f"Unknown discount: {t}")
+        return discount
 
 class ShoppingCart:
     def __init__(self):
         self.items: list[CartItem] = []
-        self._totalPrice = 0
 
     def add_item(self, item: CartItem): 
         self.items.append(item)
-        self._totalPrice += item.get_price()
 
-    def apply_discount(self, discount_type: str):
+    def get_total(self):
+        return sum(item.get_price() for item in self.items)
+
+    def apply_discount(self, discount_type: list[str]):
         try:
             # The Factory handles the logic. The Cart just uses the result!
             discount = DiscountFactory.create(discount_type)
-            self._totalPrice = discount.apply(self._totalPrice)
-            print(f"{discount_type} discount applied successfully!")
+            return discount.apply(self.get_total())
         except ValueError as e:
             print(e)
 
@@ -63,31 +81,34 @@ class Customer:
         self.shopping_cart = ShoppingCart()
     def add_item_to_cart(self, item: CartItem):
         self.shopping_cart.add_item(item)
-    def get_discount(self, discount: str):
-        self.shopping_cart.apply_discount(discount)
 
 class ProductFactory:
     @staticmethod
-    def create(type: str)-> Product:
-        if type == "iphone":
+    def create(product_type: str)-> Product:
+        if product_type == "iphone":
             return Product("iphone", 999.99)
-        elif type == "macbook":
+        elif product_type == "macbook":
             return Product("macbook", 1499.99)
         raise ValueError("Unexpected product type")
 
 class ShopFacade:
-    def add_to_cart(self, c: Customer, p_name: str, qnt: int):
+    @staticmethod
+    def add_to_cart(c: Customer, p_name: str, qnt: int):
         p = ProductFactory.create(p_name)
         ci = CartItem(p, qnt)
         c.add_item_to_cart(ci)
-    def apply_discount(self, c: Customer, discount_type: str):
-        c.get_discount(discount_type)
+    @staticmethod
+    def apply_discount(c: Customer, discount_type: list[str]):
+        return c.shopping_cart.apply_discount(discount_type)
+    @staticmethod
+    def get_total(c: Customer):
+        return c.shopping_cart.get_total()
 
 # now we can hide a lot of complexity
-shop = ShopFacade()
 c0 = Customer("Taregh", 20)
-shop.add_to_cart(c0, "macbook", 3)
-shop.apply_discount(c0, "Student")
+ShopFacade.add_to_cart(c0, "macbook", 3)
+price_b4_discount = ShopFacade.get_total(c0)
+discounted_price = ShopFacade.apply_discount(c0, ["Student", "FirstTimeUser"])
 
 
 p1 = ProductFactory.create("iphone")
